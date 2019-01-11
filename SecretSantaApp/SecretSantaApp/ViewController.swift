382fc6e9572080eac42d9451507cc8d68694c4c4
Reka <rekapap28@gmail.com>
2019-01-11 22:31:21 +0000

    adds xcode project

---
+++ SecretSantaApp/SecretSantaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecretSantaApp
+//
+//  Created by Réka Pap on 11/01/2019.
+//  Copyright © 2019 rekapap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
